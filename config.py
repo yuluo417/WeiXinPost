35c2bfb69970a766323ff68a72835631af19ed49
yuluo417 <137604028+yuluo417@users.noreply.github.com>
2025-03-21 17:26:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # 公众号配置
 # 公众号appId
-app_id = "wx******************"
+app_id = "wx4df60f103508d6e0"
 # 公众号appSecret
-app_secret = "f9a80260d33******************"
+app_secret = "27ee587562ba60b6b7207d8c56b32268"
 # 模板消息id
 # 每日消息
-template_id1 = "wX-************************************"
+template_id1 = "Peu7394u8mLYkktLrt1m1eirJ8PMwLfiaLrw_RvVjhg"
 # 课程消息,上课提醒
 template_id2 = "loe1yHWr************************************"
 # 晚安心语
 template_id3 = "-fun9-2************************************"
 # 接收公众号消息的微信号
 # 这是openid
-user = ["ojhdR50T******************"]
+user = ["ooxKK6pLf5dL6XWyIfpEdPJ0kXlc"]
 
 # 信息配置
 # 所在省份
